8fab19193572a799b402706a1831463230177496
kat-ki <glraskat@gmail.com>
2023-03-29 09:51:54 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import './App.css';
+import TodoList from "./TodoList";
+import {TaskType} from "./TodoList";
+
+function App() {
+    const todoListTitle_1: string = "What to learn"
+    const todoListTitle_2: string = "What to buy"
+
+    const tasks_1: Array<TaskType> = [
+        {id: 1, title: "HTML&CSS", isDone: true},
+        {id: 2, title: "JS", isDone: true},
+        {id: 3, title: "React", isDone: false}
+        ]
+    const tasks_2: Array<TaskType> = [
+        {id: 1, title: "Bread", isDone: true},
+        {id: 2, title: "Water", isDone: false},
+        {id: 3, title: "Salt", isDone: true}
+    ]
+    return (
+        <div className= "App">
+            <TodoList
+                title={todoListTitle_1}
+                tasks={tasks_1}/>
+            <TodoList
+                title={todoListTitle_2}
+                tasks={tasks_2}/>
+
+        </div>
+    );
+}
+
+export default App;
